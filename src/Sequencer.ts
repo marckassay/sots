b2af9baeac49797f2d98726582fb2b43b3d53b39
Marc Kassay <marckassay@gmail.com>
2017-11-19 18:58:18 -0500

    Adding group method.

--- src/Sequencer.ts
+++ src/Sequencer.ts
@@ -12,7 +12,15 @@ export interface SegmentType<T extends TimeSegment> {
 
 export interface SegmentInterface {
     add<T extends TimeSegment>(ctor: SegmentType<T>, config: TimeConfig): T;
-} 
+    group(intervals: number, ...segments: GroupParameter[]): TimeSegment;
+}
+
+export interface GroupParameter {
+    // TODO: need to figure this typing out
+    //ctor: SegmentType<T extends TimeSegment>;
+    ctor: SegmentType<CountdownSegment|CountupSegment>;
+    config: TimeConfig;
+}
 
 export class Sequencer implements SegmentInterface {
     pauser: Subject<boolean>;
@@ -30,9 +38,11 @@ export class Sequencer implements SegmentInterface {
          return segment;
     }
 
-    start() {
-        console.log("Sequencer.start() called");
+    group(intervals: number, ...segments: GroupParameter[]): TimeSegment {
+        throw new Error("Method not implemented.");
+    }
 
+    start() {
         if(!this.source) {
             this.source = SegmentCollection.getInstance().toSequencedObservable();
             
@@ -87,12 +97,14 @@ export class SegmentCollection {
         this.observables.push(segment.source);
     }
 }
-/* 
-export function add<T extends TimeSegment>(ctor: SegmentType<T>, config: TimeConfig): T {
-    return new ctor(config);
+
+// simply a pass-thru function to group function...
+export function add<T extends TimeSegment>(ctor: SegmentType<T>, config: TimeConfig): GroupParameter {
+    return {ctor, config};
 } 
-*/
+
 export class TimeSegment implements SegmentInterface {
+
     source: Observable<number>;
 
     constructor (public config: TimeConfig) {
@@ -111,6 +123,20 @@ export class TimeSegment implements SegmentInterface {
         SegmentCollection.getInstance().push(segment);
         return segment;
     }
+
+    group(intervals: number, ...segments: GroupParameter[]): TimeSegment {
+        let segment: TimeSegment;
+
+        for (let index = 0; index < intervals; index++) {
+            for (let segmentIndex = 0; segmentIndex < segments.length; segmentIndex++) {
+                const segType: GroupParameter = segments[segmentIndex];
+                segment = new segType.ctor(segType.config);
+                SegmentCollection.getInstance().push(segment);
+            }
+        }
+
+        return segment;
+    }
 }
 export class CountdownSegment extends TimeSegment {
     constructor (public config: TimeConfig) {
@@ -124,8 +150,9 @@ export class CountupSegment extends TimeSegment {
 }
 
 const sequencer: Sequencer = new Sequencer({period: 1000});
-sequencer.add(CountdownSegment, {period: 3000})
-         .add(CountupSegment, {period: 2000});
+sequencer.add(CountdownSegment, {period: 5000})
+         .group(3, add(CountdownSegment, {period: 1000}), add(CountdownSegment, {period: 2000}))
+         .add(CountupSegment, {period: 5000});
 
 sequencer.start();
 
